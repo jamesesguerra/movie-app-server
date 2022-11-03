2d8d2286053d9247624e101a763b6eb15eadb96a
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-11-03 23:20:42 +0800

    add login endpoint

---
+++ controllers/login.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user");
+
+
+router.post("/", async(req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return res.status(401).json({
+      error: "invalid email or password"
+    });
+  }
+
+  const userForToken = {
+    email: user.email,
+    id: user._id
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  res
+    .status(200)
+    .send({ token, email: user.email });
+});
+
+module.exports = router;
